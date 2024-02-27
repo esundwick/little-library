6a699783e81628b999ec4f417be6799a6f0f2431
Evan Sundwick <esundwick@users.noreply.github.com>
2024-02-27 09:57:29 -0500

    adding sanity to the project

---
+++ sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '1w4k6s7b',
+    dataset: 'production'
+  }
+})
---
+++ sanity/sanity.config.ts
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'Little Library',
+
+  projectId: '1w4k6s7b',
+  dataset: 'production',
+
+  plugins: [
+    structureTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
